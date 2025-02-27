506c29d53a17c2e4f605ef9bffaf9acaff048f8e
unknown <1720811650@qq.com>
2025-02-27 13:24:29 +0000

    Add API endpoint for posting a new movie.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -70,10 +70,23 @@ export class RestAPIStack extends cdk.Stack {
             resources: [moviesTable.tableArn],
           }),
         });
+
+        const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_16_X,
+          entry: `${__dirname}/../lambdas/addMovie.ts`,
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+          environment: {
+            TABLE_NAME: moviesTable.tableName,
+            REGION: "eu-west-1",
+          },
+        });
         
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
+        moviesTable.grantReadWriteData(newMovieFn)
         
         // REST API 
         const api = new apig.RestApi(this, "RestAPI", {
@@ -100,6 +113,13 @@ export class RestAPIStack extends cdk.Stack {
           "GET",
           new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
         );
+
+        moviesEndpoint.addMethod(
+          "POST",
+          new apig.LambdaIntegration(newMovieFn, { proxy: true })
+        );
+
+        
       }
     }
     
